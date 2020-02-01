42b9995fdc7fe19987be0e62215cc975c69cb8a8
Lauren Dawson <dawsonlaurene@gmail.com>
2020-02-01 12:16:04 -0600

    cleaned up code

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -64,16 +64,8 @@ public class RobotContainer
   private void configureButtonBindings()
   {
       // Bind controller buttons to commands
-      new JoystickButton(driver1Joystick, GamePadButtons.back.value).whenPressed(new LaunchEnergyCommand(m_energyLaunchSubsystem, 0.0, 0.0));
-      //new JoystickButton(m_driver1Joystick, GamePadButtons.leftStickPress.value).whenPressed(new LaunchEnergyCommand(m_energyLaunchSubsystem, 0.0));
-      //new JoystickButton(m_driver1Joystick, GamePadButtons.bumperL.value).whenPressed(new LaunchEnergyCommand(m_energyLaunchSubsystem, 0.65));
-      //new JoystickButton(m_driver1Joystick, GamePadButtons.back.value).whenPressed(new LaunchEnergyCommand(m_energyLaunchSubsystem, 0.7));
-      // new JoystickButton(m_driver1Joystick, GamePadButtons.start.value).whenPressed(new LaunchEnergyCommand(m_energyLaunchSubsystem, 0.75));
-      new JoystickButton(driver1Joystick, GamePadButtons.bumperR.value).whenPressed(new LaunchEnergyCommand(m_energyLaunchSubsystem, 0.6, 0.6));
-      new JoystickButton(driver1Joystick, GamePadButtons.buttonY.value).whenPressed(new LaunchEnergyCommand(m_energyLaunchSubsystem, 0.7, 0.7));
-      new JoystickButton(driver1Joystick, GamePadButtons.buttonB.value).whenPressed(new SpinRecordPlayer(recordPlayer, 0.8));
-      new JoystickButton(driver1Joystick, GamePadButtons.buttonA.value).whenPressed(new LaunchEnergyCommand(m_energyLaunchSubsystem, 0.9, 0.9));
-      new JoystickButton(driver1Joystick, GamePadButtons.buttonX.value).whenPressed(new LaunchEnergyCommand(m_energyLaunchSubsystem, 1.0, 1.0));
+      new JoystickButton(driver1Joystick, GamePadButtons.buttonB.value).whenPressed(new SpinRecordPlayer(recordPlayer, 0.0));
+      new JoystickButton(driver1Joystick, GamePadButtons.buttonX.value).whenPressed(new SpinRecordPlayer(recordPlayer, 0.8));
 
       //m_driveTrainSubsystem.runDriveMotorsStraight(ProAxis.YAxis.value);
       //m_driveTrainSubsystem.runDriveMotorsTurn(ProAxis.XAxis.value);
