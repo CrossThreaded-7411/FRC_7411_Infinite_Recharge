b97f033df16cba5b7fac2485e27e05fbe5da7901
Ryley Hindman <cessnageek@gmail.com>
2020-02-09 01:30:25 -0600

    Might work. Basically copied code from online.  Added gain setter and am using ByteBuffer.

--- src/main/java/frc/robot/subsystems/TCS3472Subsystem.java
+++ src/main/java/frc/robot/subsystems/TCS3472Subsystem.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems;
 
+import java.nio.ByteBuffer;
+
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -10,16 +12,19 @@ public class TCS3472Subsystem extends SubsystemBase {
     private int id;
     private boolean enabled;
 
-    private final int ENABLE = 0x80; // 0x00
-    private final int ATIME = 0x81; // 0x01
-    private final int CDATAL = 0xB4; // 0x14 0xB4 0x94
-    private final int CDATAH = 0xB5; // 0x15
-    private final int RDATAL = 0xB6; // 0x16 0xB6 0x96
-    private final int RDATAH = 0xB7; // 0x17
-    private final int GDATAL = 0xB8; // 0x18 0xB8 0x98
-    private final int GDATAH = 0xB9; // 0x19
-    private final int BDATAL = 0xBA; // 0x1A 0xBA 0x9A
-    private final int BDATAH = 0xBB; // 0x1B
+    private final int ENABLE = 0x00; // 0x00
+    private final int ATIME = 0x01; // 0x01
+    private final int CDATAL = 0x14; // 0x14 0xB4 0x94
+    private final int CDATAH = 0x15; // 0x15
+    private final int RDATAL = 0x16; // 0x16 0xB6 0x96
+    private final int RDATAH = 0x17; // 0x17
+    private final int GDATAL = 0x18; // 0x18 0xB8 0x98
+    private final int GDATAH = 0x19; // 0x19
+    private final int BDATAL = 0x1A; // 0x1A 0xBA 0x9A
+    private final int BDATAH = 0x1B; // 0x1B
+    private final int GAIN = 0x0F;
+    private final int COMMANDBIT = 0x80;
+    private final int AUTOINCREMENT = 0x20;
 
     public TCS3472Subsystem(int deviceAddress, int id){
         System.out.println("Entered TCS subsystem constructor");
@@ -29,27 +34,31 @@ public class TCS3472Subsystem extends SubsystemBase {
         i2cSource = new I2C(I2C.Port.kOnboard, this.deviceAddress);
         enabled = false;
         System.out.println("Finished TCS subsystem constructor");
-
     }
 
     public void enable() {
-        i2cSource.write(ENABLE, 3); // 00000011 enables RGBC and Power ON
+        write8(ENABLE, 3);  // 00000011 enables RGBC and Power ON
         enabled = true;
         System.out.println("Enabled TCS");
     }
 
     public void disable() {
-        i2cSource.write(ENABLE, 0); // 00000000 disables RGBC and Power ON
+        write8(ENABLE, 0);
         enabled = false;
     }
 
     public void setATIME() {
-        i2cSource.write(ATIME, 246); // 0xF6 ----- Integration time = 24ms
+        write8(ATIME, 246);
+    }
+
+    public void setGAIN() {
+        write8(GAIN, 0x01);
     }
 
     public void init() {
-        enable();
         setATIME();
+        setGAIN();
+        enable();
     }
 
     public int[] getData() {
@@ -65,35 +74,10 @@ public class TCS3472Subsystem extends SubsystemBase {
         System.out.println(chipId[0]);
         int[] output= {-1,-1,-1,-1};
         if(enabled == true) {
-            System.out.println("getting TCS");
-            ByteBuffer clearBuffer = ByteBuffer.allocate(2);
-            ByteBuffer redBuffer = ByteBuffer.allocate(2);
-            ByteBuffer blueBuffer = ByteBuffer.allocate(2);
-            ByteBuffer greenBuffer = ByteBuffer.allocate(2);
-            // byte[] clearBuffer = {0,0};
-            // byte[] redBuffer = {0,0};
-            // byte[] greenBuffer = {0, 0};
-            // byte[] blueBuffer = {0, 0};
-
-            i2cSource.read(CDATAL, 2, clearBuffer);
-            i2cSource.read(RDATAL, 2, redBuffer);
-            i2cSource.read(GDATAL, 2, greenBuffer);
-            i2cSource.read(BDATAL, 2, blueBuffer);
-
-            byte clearL = clearBuffer.get();
-            byte clearH = clearBuffer.get();
-            byte redL = redBuffer.get();
-            byte redH = redBuffer.get();
-            byte greenL = greenBuffer.get();
-            byte greenH = greenBuffer.get();
-            byte blueL = blueBuffer.get();
-            byte blueH = blueBuffer.get();
-
-            System.out.println("RedHigh: " + redH + "RedLow: " + redL);
-
-            int redValue = (((redH & 0xFF) << 8) | (redL & 0xFF));  // Bitshifts the high buffer over 8 and appends the low buffer.
-            int greenValue = (((greenH & 0xFF) << 8) | (greenL & 0xFF));
-            int blueValue = (((blueH & 0xFF) << 8) | (blueL & 0xFF));
+            int redValue = read16(RDATAL);
+            int blueValue = read16(BDATAL);
+            int greenValue = read16(GDATAL);
+            int clearValue = read16(CDATAL);
 
             output[0] = 0;
             output[1] = redValue;
@@ -108,6 +92,20 @@ public class TCS3472Subsystem extends SubsystemBase {
         }
 
         return output;
+    }
+
+    private int read16(int register) {
+        ByteBuffer rawByte = ByteBuffer.allocate(2);
+        i2cSource.read(COMMANDBIT | AUTOINCREMENT | register, 2, rawByte);
+        byte lo = rawByte.get();
+        byte hi = rawByte.get();
+
+        int result = ((hi & 0xFF) << 8) | (lo & 0xFF);
+        return result;
+    }
+
+    private void write8(int register, int value){
+        i2cSource.write(COMMANDBIT | register, (byte) (value & 0xFF));
     }
         
 
