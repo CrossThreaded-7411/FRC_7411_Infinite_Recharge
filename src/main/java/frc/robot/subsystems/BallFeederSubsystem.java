6ae46317b4786a497b5a38bb0c49c012005d2c1e
Jeff Dawson <dawsonjeffreyd@johndeere.com>
2020-02-11 12:48:11 -0600

    Missing FIles
    Added missing REVRobotics json file and BallFeederSubsystem file

---
+++ src/main/java/frc/robot/subsystems/BallFeederSubsystem.java
@@ -0,0 +1,45 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved. */
+/* Open Source Software - may be modified and shared by FRC teams. The code */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project. */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.CANID;
+
+public class BallFeederSubsystem extends SubsystemBase
+{
+   private VictorSPX feederMotor = new VictorSPX(CANID.ballFeeder);
+
+   /**
+    * Creates a new DriveSubsystem.
+    */
+   public BallFeederSubsystem()
+   {
+      // Constructor
+      stopMotor();
+   }
+
+   /**
+    * Sets motor power for the ball launcher motor
+    * 
+    * @param targetPower Accepts a power level between -1 to 1
+    */
+   public void setMotorPower(double power)
+   {
+      feederMotor.set(ControlMode.PercentOutput, power);
+   }
+
+   /**
+    * Stops the motor by setting power to 0.0
+    */
+   public void stopMotor()
+   {
+      feederMotor.set(ControlMode.PercentOutput, 0.0);
+   }
+}
