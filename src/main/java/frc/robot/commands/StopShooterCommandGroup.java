05fcd4e88bd730f9566e828e9d94cc415723c62f
Ainsley Hindman <violingirl40@gmail.com>
2020-02-15 13:56:35 -0600

    no message

---
+++ src/main/java/frc/robot/commands/StopShooterCommandGroup.java
@@ -0,0 +1,19 @@
+package frc.robot.commands;
+
+import frc.robot.subsystems.BallShooterSubsystem;
+import frc.robot.subsystems.BallFeederSubsystem;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+
+public class StopShooterCommandGroup extends SequentialCommandGroup
+{
+    public StopShooterCommandGroup(BallShooterSubsystem ballShooter, BallFeederSubsystem ballFeeder)
+    {
+        // new SequentialCommandGroup(new RunBallFeeder(ballFeeder, 0.5), new WaitCommand(1), new RunBallShooter(ballShooter, 0.5, 0.7));
+        addCommands(
+            new RunBallShooter(ballShooter, 0.0, 0.0),
+            new RunBallFeeder(ballFeeder, 0.0)
+        );
+    }
+}
