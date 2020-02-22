003ea99e27072f4f90993bf12d3bcaac24a9a2ab
Lauren Dawson <dawsonlaurene@gmail.com>
2020-02-22 15:11:09 -0600

    Got rid of the unused buttons

--- src/main/java/frc/robot/commands/StopShooterCommandGroup.java
+++ src/main/java/frc/robot/commands/StopShooterCommandGroup.java
@@ -1,19 +1,22 @@
 package frc.robot.commands;
 
 import frc.robot.subsystems.BallShooterSubsystem;
+import frc.robot.subsystems.BallCollectorSubsystem;
+import frc.robot.commands.BallCollectorManual;
 import frc.robot.subsystems.BallFeederSubsystem;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 
 public class StopShooterCommandGroup extends SequentialCommandGroup
 {
-    public StopShooterCommandGroup(BallShooterSubsystem ballShooter, BallFeederSubsystem ballFeeder)
+    public StopShooterCommandGroup(BallShooterSubsystem ballShooter, BallFeederSubsystem ballFeeder, BallCollectorSubsystem ballCollector)
     {
         // new SequentialCommandGroup(new RunBallFeeder(ballFeeder, 0.5), new WaitCommand(1), new RunBallShooter(ballShooter, 0.5, 0.7));
-        addCommands(
+        addCommands
+        (
             new RunBallShooter(ballShooter, 0.0, 0.0),
-            new RunBallFeeder(ballFeeder, 0.0)
+            new RunBallFeeder(ballFeeder, 0.0),
+            new BallCollectorManual(ballCollector, 0.0)        
         );
     }
 }
