f5b9e0d6c0a22dd507f1f3de3e847749b34da14c
Ainsley Hindman <violingirl40@gmail.com>
2020-02-23 18:05:22 -0600

    working on autonomous, does not work yet.

---
+++ src/main/java/frc/robot/commands/AutonomousDriveCommandGroup.java
@@ -0,0 +1,24 @@
+// package frc.robot.commands;
+
+//  import edu.wpi.first.wpilibj2.command.WaitCommand;
+//  import frc.robot.subsystems.DriveTrainSubsystem;
+//  import edu.wpi.first.wpilibj2.command.CommandGroupBase;
+//  import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+
+//  public class AutonomousDrive extends SequentialCommandGroup
+//  {
+//      public AutonomousDrive(DriveTrainSubsystem drivetrain)
+//      {
+//          addCommands
+//          (
+//         new DriveByJoystick(drivetrain, 0.1,0.1 )
+//         new WaitCommand(2),
+
+            
+
+//          );
+
+//     }
+
+//  }
